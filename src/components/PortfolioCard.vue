4cea884e89015b99af17e38790fa889152294414
FelineJTD <16520335@std.stei.itb.ac.id>
2022-06-16 14:57:10 +0700

    try portfolio

---
+++ src/components/PortfolioCard.vue
@@ -0,0 +1,49 @@
+<script lang="ts">
+import { defineProps, defineComponent } from "vue";
+
+export default defineComponent({
+  name: 'PortfolioCard',
+  data() {
+    return {
+      play: false,
+    };
+  },
+  props: {
+    title: String,
+    desc: String,
+    link: String,
+    image: String,
+    video: String,
+  },
+  methods: {
+    onMouseEnter() {
+      this.play = true;
+    },
+    onMouseLeave() {
+      this.play = false;
+    },
+  },
+});
+
+defineProps<{ 
+  title: string,
+  desc: string,
+  link: string,
+  image: string,
+  video: string,
+}>()
+
+</script>
+
+<template>
+  <div class="rounded-xl bg-coral-500 bg-[url('./assets/texture-25.png')] bg-blend-overlay relative mr-12 min-w-[30vw] text-center p-[2%] shadow-xl text-white" @mouseenter="onMouseEnter" @mouseleave="onMouseLeave">
+    <div class="relative shadow-xl">
+      <!-- <img :src="image" class="rounded-xl" /> -->
+      <img :src="image" class="rounded-xl" />
+      <video :src="video" v-if="play" class="absolute rounded-xl bottom-0" autoplay loop muted />
+    </div>
+    <h1 class="mt-6">{{ title }}</h1>
+    <a :href="link" target="_blank" class="text-teal-t hover:text-coral-300 hover:underline duration-300"><p class="font-bold">{{ link }}</p></a>
+    <p class="mt-6">{{ desc }}</p>
+  </div>
+</template>
--- src/views/Portfolio.vue
+++ src/views/Portfolio.vue
@@ -1,10 +1,17 @@
 <script lang="ts">
+import PortfolioThumbnail from "../components/PortfolioThumbnail.vue";
+import uniqueId from "lodash/uniqueId";
 export default {
-  name: 'Portfolio',
-  data() {
-    return {
-    };
-  },
+    name: "Portfolio",
+    data() {
+        return {
+          portfolios: [
+            { id: uniqueId('portfolio-'), title: 'Wispril ITB 2022', desc: 'A graduation website created to celebrate the April 2022 graduation of ITB students. This website includes features like wall of fame, success stories, and more.', link: 'https://paradewisudaitb.com', image: '/portfolio-thumbnail/wispril.png', video: '/portfolio-thumbnail/wispril.mp4'},
+            { id: uniqueId('portfolio-'), title: 'Wispril ITB 2022', desc: 'A graduation website created to celebrate the April 2022 graduation of ITB students. This website includes features like wall of fame, success stories, and more.', link: 'https://paradewisudaitb.com', image: '/portfolio-thumbnail/wispril.png', video: '/portfolio-thumbnail/wispril.mp4'}
+          ]
+        };
+    },
+    components: { PortfolioThumbnail }
 }
 
 </script>
@@ -12,43 +19,8 @@ export default {
 <template>
   <div class="min-h-screen bg-coral-100 bg-texture bg-blend-overlay pt-12 px-[8%]">
     <img src="../assets/portfolio/Browse_My_Web_Portfolio.svg" alt="Browse my web portfolio" class="h-[3rem] my-16">
-    {{ aaa }}
+    <section class="flex">
+      <PortfolioThumbnail v-for="portfolio in portfolios" :key='portfolio.id' :title="portfolio.title" :desc="portfolio.desc" :link="portfolio.link" :image="portfolio.image" :video="portfolio.video" />
+    </section>
   </div>
 </template>
-
-<style>
-html,
-body {
-  scroll-behavior: smooth;
-  scrollbar-color: #1D3935 #C2685B;
-  scrollbar-width: thin;
-}
-
-body::-webkit-scrollbar {
-  width: 0.5em;
-}
-
-body::-webkit-scrollbar-track {
-  box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
-  background-color: #C2685B;
-}
-
-body::-webkit-scrollbar-thumb {
-  background-color: #1D3935;
-}
-
-#portfolio {
-  -ms-overflow-style: none; 
-  scrollbar-width: none;
-  overflow: -moz-scrollbars-none;
-}
-
-#portfolio::-webkit-scrollbar {
-  display: none;
-}
-
-.vertical-rl {
-  writing-mode: vertical-rl;
-}
-
-</style>
