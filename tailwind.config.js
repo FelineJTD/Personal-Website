f1849028fb98fe972037828d86da41a014a56604
FelineJTD <16520335@std.stei.itb.ac.id>
2022-06-17 17:03:57 +0700

    add not found page

--- tailwind.config.js
+++ tailwind.config.js
@@ -31,15 +31,6 @@ module.exports = {
         '200': '#DE9C65',
         't': '#44312E'
       }
-      // 'blue': '#1fb6ff',
-      // 'purple': '#7e5bef',
-      // 'pink': '#ff49db',
-      // 'orange': '#ff7849',
-      // 'green': '#13ce66',
-      // 'yellow': '#ffc82c',
-      // 'gray-dark': '#273444',
-      // 'gray': '#8492a6',
-      // 'gray-light': '#d3dce6',
     },
     fontFamily: {
       sans: ['Quicksand', 'sans-serif'],
