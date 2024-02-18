3d65b9e290876b799b1f7e1568917672162cc9fa
Felicia Sutandijo <75204822+FelineJTD@users.noreply.github.com>
2024-02-19 02:18:23 +0700

    Update Countdown.vue

--- src/views/Countdown.vue
+++ src/views/Countdown.vue
@@ -5,7 +5,7 @@ export default {
     return {
       real_start_date: new Date("2023-08-21"),
       start_date: new Date() > new Date("2023-08-21") ? new Date() : new Date("2023-08-21"),
-      end_date: new Date("2024-02-06"),
+      end_date: new Date("2024-10-31"),
     };
   },
   methods: {
@@ -32,7 +32,7 @@ export default {
   <div class="flex flex-col justify-center items-center py-12 px-6 min-h-screen bg-coral-100">
     <h1 class="text-center mb-12">
       <span class="text-teal-t text-[5rem] leading-snug">{{daysLeft()}}<br /></span>
-      <span class="text-coral-t text-lg">days left in the Netherlands</span>
+      <span class="text-coral-t text-lg">days left until Irfan comes home (maybe).</span>
     </h1>
 
     <p class="text-teal-t text-center">which roughly equals to</p>
