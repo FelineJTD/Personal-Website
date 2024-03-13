6e5bbc6afb2e300ea62e03cbd055557cad1374bf
FelineJTD <13520050@std.stei.itb.ac.id>
2024-03-13 23:17:42 +0700

    chore: delete console.log

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -2,8 +2,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
-console.log(import.meta.env.VITE_API_KEY);
-
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_API_KEY,
   authDomain: import.meta.env.VITE_AUTH_DOMAIN,
